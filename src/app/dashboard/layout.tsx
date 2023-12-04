fb32b6fbe12ae955c813f6450fcb59f51524b5ee
kartikey2308 <kartikey.a.mishra@accenture.com>
2023-12-04 13:56:52 +0530

    added header

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,11 @@
-import DashboardHeader from "../../components/header/DashboardHeader"
+"use client"
+import DashboardHeader from "../../components/header/DashboardHeader";
 
-
-export default function Layout({ children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <div><DashboardHeader/>
-        {children}
-        <h1 className="text-3xl font-bold underline">Hello World!</h1>
-        </div>
-    )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <DashboardHeader />
+      {children}
+    </div>
+  );
 }
