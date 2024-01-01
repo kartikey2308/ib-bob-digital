42fa09a03d1e72a3f0d9b0a58f9cba88f2fb4ca1
priyanka-d-shetty <priyanka.d.shetty@accenture.com>
2024-01-01 15:44:47 +0530

    header test case

--- src/_tests_/components/Header/Header.test.tsx
+++ src/_tests_/components/Header/Header.test.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import {render} from "@testing-library/react";
+import {render,screen} from "@testing-library/react";
 import Header from "../../../components/Header/Header"
 describe("Header", () => {
   it("renders the header", () => {
     const header = render( <Header/>);
     expect(header).toBeDefined();
   });
+  it("renders an input search bar",()=>{
+    render(<Header/>);
+    const searchInput = screen.getAllByPlaceholderText(/search/i);
+    expect(searchInput).toBeDefined();
+  })
+  it("renders notification image",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("notification");
+    expect(images).toBeDefined();
+  })
+  it("renders add image",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("add");
+    expect(images).toBeDefined();
+  })
+  it("renders info image",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("info");
+    expect(images).toBeDefined();
+  })
+  it("renders Search Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("Search Icon");
+    expect(images).toBeDefined();
+  })
+  it("renders Mic Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("Mic Icon");
+    expect(images).toBeDefined();
+  })
+  it("renders BobLogo Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("BobLogo");
+    expect(images).toBeDefined();
+  })
+  it("renders more Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("more");
+    expect(images).toBeDefined();
+  })
+  it("renders profile Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("profile");
+    expect(images).toBeDefined();
+  })
+  it("renders issues Icon",()=>{
+    render(<Header/>);
+    const images = screen.getAllByAltText("issues");
+    expect(images).toBeDefined();
+  })
 });
